31a144c8fdb1c6d507a2645c844891ccca916b04
Lucas Rocha <santoslucas.dev@gmail.com>
2022-12-30 13:38:25 -0300

    implementing next pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'productionß'
+})
 
+const nextConfig = withPWA({
+  // next config
+})
 module.exports = nextConfig
