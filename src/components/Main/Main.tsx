96b55d99f25b63a1205caac3420bdb6bd27be583
Lucas Rocha <santoslucas.dev@gmail.com>
2022-12-30 13:02:56 -0300

    changing storybook knobs to interactions

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,15 +1,16 @@
 import * as S from './styles'
 
-const Main = () => (
+const Main = ({
+  title = 'React Avançado',
+  description = 'Typescript, ReactJS, NextJS e Styled Components'
+}) => (
   <S.Wrapper>
     <S.Logo
       src="/img/logo.svg"
       alt="imagem de um átomo em rosa com texto React Avançado em branco"
     />
-    <S.Title>React Avançado</S.Title>
-    <S.Description>
-      Typesript, ReactJS, NextJS e Styled Components
-    </S.Description>
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
     <S.Illustration
       src="img/hero-illustration.svg"
       alt="Imagem de um programador de frente para o monitor"
