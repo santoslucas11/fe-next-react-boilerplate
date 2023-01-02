60ff8454aa1d5b5cfe9383d5953bbaca6652449b
Lucas Rocha <santoslucas.dev@gmail.com>
2023-01-02 12:27:56 -0300

    general adjustments

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,21 +1,20 @@
-import * as S from './styles'
+import * as S from './Main.styles'
 
-const Main = ({
-  title = 'React Avançado',
-  description = 'Typescript, ReactJS, NextJS e Styled Components'
-}) => (
-  <S.Wrapper>
-    <S.Logo
-      src="/img/logo.svg"
-      alt="imagem de um átomo em rosa com texto React Avançado em branco"
-    />
-    <S.Title>{title}</S.Title>
-    <S.Description>{description}</S.Description>
-    <S.Illustration
-      src="img/hero-illustration.svg"
-      alt="Imagem de um programador de frente para o monitor"
-    />
-  </S.Wrapper>
-)
-
-export default Main
+export default function Main2() {
+  return (
+    <S.Wrapper>
+      <S.Logo
+        src="/img/logo.svg"
+        alt="imagem de um átomo em rosa com texto React Avançado em branco"
+      />
+      <S.Title>React Avançado</S.Title>
+      <S.Description>
+        Typescript, ReactJS, NextJS e Styled Components
+      </S.Description>
+      <S.Illustration
+        src="img/hero-illustration.svg"
+        alt="Imagem de um programador de frente para o monitor"
+      />
+    </S.Wrapper>
+  )
+}
