80aa942706d447a4c991bdb2a44cddd79101c780
Lucas Rocha <santoslucas.dev@gmail.com>
2023-01-02 01:30:12 -0300

    general adjustments

---
+++ generators/plopfile.js
@@ -0,0 +1,39 @@
+module.exports = (plop) => {
+  plop.setGenerator('component', {
+    description: 'Create a component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your component name?'
+      }
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.tsx',
+        templateFile: 'templates/Component.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/index.tsx',
+        templateFile: 'templates/Component.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.styles.ts',
+        templateFile: 'templates/styles.ts.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.stories.tsx',
+        templateFile: 'templates/stories.tsx.hbs'
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/{{pascalCase name}}.spec.tsx',
+        templateFile: 'templates/test.tsx.hbs'
+      }
+    ]
+  })
+}
